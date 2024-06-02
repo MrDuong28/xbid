fabf2c666eeab2572a7c6c06973d58e476110ed5
MrDuong28 <150405401+MrDuong28@users.noreply.github.com>
2024-06-02 15:37:37 +0700

    firt-commit

---
+++ backend/xbid-main/router/product.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+  AddCategory,
+  AddProduct,
+  GetAllProduct,
+} from "../controllers/product.js";
+const router = express.Router();
+router.get("/products", GetAllProduct);
+router.post("/category", AddCategory);
+router.post("/products", AddProduct);
+export default router;
